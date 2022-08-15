a5da209fba40ad52c29fbce4c5c4736489f84c3a
Mike Landau <mike@jetpack.io>
2022-08-15 11:49:18 -0700

    [envsec] Set secret from file DEV-1015 (#2458)
    ## Summary
    
    Castor needs this
    
    ## How was it tested?
    
    `jetpack env set foo-secret=@foo.secret`
    
    ## Is this change backwards-compatible?

--- envcli/set.go
+++ envcli/set.go
@@ -4,6 +4,7 @@
 package envcli
 
 import (
+	"os"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -16,7 +17,7 @@ func SetCmd(cmdCfg *CmdConfig) *cobra.Command {
 	command := &cobra.Command{
 		Use:   "set <NAME1>=<value1> [<NAME2>=<value2>]...",
 		Short: "Securely store one or more environment variables",
-		Long:  "Securely store one or more environment variables.",
+		Long:  "Securely store one or more environment variables. To test contents of a file as a secret use set=@<file>",
 		Args:  cobra.MinimumNArgs(1),
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			for _, arg := range args {
@@ -32,13 +33,11 @@ func SetCmd(cmdCfg *CmdConfig) *cobra.Command {
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			envMap := map[string]string{}
-			for _, arg := range args {
-				k, v, _ := strings.Cut(arg, "=")
-				envMap[k] = v
+			envMap, err := parseArgs(args)
+			if err != nil {
+				return errors.WithStack(err)
 			}
-
-			err := SetEnvMap(cmd.Context(), cmdCfg.Store, cmdCfg.EnvId, envMap)
+			err = SetEnvMap(cmd.Context(), cmdCfg.Store, cmdCfg.EnvId, envMap)
 			if err != nil {
 				return errors.WithStack(err)
 			}
@@ -57,4 +56,33 @@ func SetCmd(cmdCfg *CmdConfig) *cobra.Command {
 		},
 	}
 	return command
+}
+
+func parseArgs(args []string) (map[string]string, error) {
+	envMap := map[string]string{}
+	for _, arg := range args {
+		k, v, _ := strings.Cut(arg, "=")
+		if strings.HasPrefix(v, "\\@") {
+			v = strings.TrimPrefix(v, "\\")
+		} else if strings.HasPrefix(v, "@") {
+			file := strings.TrimPrefix(v, "@")
+			if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
+				return nil, errors.Errorf(
+					"@ syntax is used for setting a secret from a file. file %s "+
+						"does not exist. If your value starts with @, escape it with "+
+						"a backslash, e.g. %s='\\%s'",
+					file,
+					k,
+					v,
+				)
+			}
+			c, err := os.ReadFile(file)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to read file %s", file)
+			}
+			v = string(c)
+		}
+		envMap[k] = v
+	}
+	return envMap, nil
 }
