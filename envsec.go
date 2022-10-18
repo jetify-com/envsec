28db02041c0e234ccd7eef8a568155079e939f18
savil <676452+savil@users.noreply.github.com>
2022-10-18 15:19:44 -0500

    [envsec] remove old comment (#2745)
    ## Summary
    
    This comment is outdated
    
    
    ## How was it tested?
    
    didn't test
    
    ## Is this change backwards-compatible?
    
    Signed-off-by: savil <676452+savil@users.noreply.github.com>

--- envsec.go
+++ envsec.go
@@ -56,7 +56,6 @@ type EnvVar struct {
 func NewStore(ctx context.Context, config Config) (Store, error) {
 	switch config.(type) {
 	case *SSMConfig:
-		// Remove org argument when fully transitioned to projects
 		return newSSMStore(ctx, config.(*SSMConfig))
 	default:
 		return nil, errors.Errorf("unsupported store type: %T", config)
